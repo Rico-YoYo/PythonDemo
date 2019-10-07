c2283a938d387679eb4af61f724aa45295169a2b
Rico-YoYo <136013560@qq.com>
2019-10-07 20:12:55 +0800

    提交新的项目：PythonDemo

---
+++ Turtle28/one.py
@@ -0,0 +1,11 @@
+__author__ = 'Rico'
+__date__ = '2019/10/6 22:02'
+
+import turtle
+
+amy = turtle.Turtle()
+amy.color("yellow")
+for side in [1, 2, 3, 4]:
+    amy.forward(100)
+    amy.right(90)
+
